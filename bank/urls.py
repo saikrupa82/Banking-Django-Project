3f728bfdd062e06633ac5d9d1cf39531283015d5
saikrupa82 <46783175+saikrupa82@users.noreply.github.com>
2021-02-16 15:56:59 +0530

    Removed url ( casuing Internal Server Error)

--- bank/urls.py
+++ bank/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,url
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static,serve
 
@@ -22,6 +22,4 @@ from django.conf.urls.static import static,serve
 urlpatterns = [
     path('',include('bankingsystem.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
